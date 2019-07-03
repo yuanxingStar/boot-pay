a3b2ad767d333b9dd258ba7420471bcbb2b5b442
yuanxing <18561703921@163.com>
2019-07-03 20:16:28 +0800

    no message

---
+++ src/main/java/org/yuanxing/bootpay/bean/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @author yuanxing
+ * @create 2019-07-03 19:02
+ * @version 0.0.1
+ * @see (1)
+ */
+package org.yuanxing.bootpay.bean;
---
+++ src/main/java/org/yuanxing/bootpay/util/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @author yuanxing
+ * @create 2019-07-03 19:04
+ * @version 0.0.1
+ * @see (1)
+ */
+package org.yuanxing.bootpay.util;
